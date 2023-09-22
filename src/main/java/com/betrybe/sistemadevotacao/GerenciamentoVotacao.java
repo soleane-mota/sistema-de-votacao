b9e92e6c87b9aa7679b816419e3371b566c1a588
Soleane Mota <sem.sol@gmail.com>
2023-09-22 15:38:53 -0400

    feat: implement registration methods in GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -13,12 +13,34 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
+    if (!this.pessoasCandidatas.isEmpty()) {
+      for (PessoaCandidata pc :
+          this.pessoasCandidatas) {
+        if (pc.getNumero() == numero) {
+          System.out.println("Número da pessoa candidata já utilizado!");
+          return;
+        }
+      }
+    }
 
+    PessoaCandidata pessoaCandidata = new PessoaCandidata(nome, numero);
+    this.pessoasCandidatas.add(pessoaCandidata);
   }
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
+    if (!this.pessoasEleitoras.isEmpty()) {
+      for (PessoaEleitora pe :
+          this.pessoasEleitoras) {
+        if (pe.getCpf() == cpf) {
+          System.out.println("Pessoa eleitora já cadastrada!");
+          return;
+        }
+      }
+    }
 
+    PessoaEleitora pessoaEleitora = new PessoaEleitora(nome, cpf);
+    this.pessoasEleitoras.add(pessoaEleitora);
   }
 
   @Override
