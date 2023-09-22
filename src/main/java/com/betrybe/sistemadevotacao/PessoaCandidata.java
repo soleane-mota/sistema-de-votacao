7c0ec1919e88a9058f967be115ae44cfcf244a39
Soleane Mota <sem.sol@gmail.com>
2023-09-22 14:43:05 -0400

    feat: create class PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,39 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * Classe PessoaCandidata.
+ */
+public class PessoaCandidata extends Pessoa {
 
+  private int numero;
+  private int votos;
+
+  /**
+   * Construtor PessoaCandidata.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    super.setNome(nome);
+    this.setNumero(numero);
+    this.setVotos(0);
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    this.votos++;
+  }
 }
